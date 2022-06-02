55918d2ab97b020c40fce3192e4d13c655a93938
sergio <dirtycrew666@gmail.com>
2022-06-02 16:05:05 +0500

    Can accept multi-digit numbers and process signle-type operations

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
 const buttons = document.querySelectorAll("button")
 const resultField = document.querySelector(".result")
+const root = document.documentElement
 
 let nums = []
-let operator 
+let operator
+let resultContent = " " 
 
 buttons.forEach(button => {
     button.addEventListener("click", function(){
         if (button.classList.contains("add")) {
+            nums.push(parseInt(resultField.textContent))
+            console.log(nums)
             operator = "+"
-            resultField.textContent = operator
+            resultField.textContent = ""
         }
         if (button.classList.contains("subtract")) {
+            nums.push(parseInt(resultField.textContent))
             operator = "-"
-            resultField.textContent = operator
+            resultField.textContent = ""
         }
         if (button.classList.contains("multiply")) {
+            nums.push(parseInt(resultField.textContent))
             operator = "*"
-            resultField.textContent = operator
+            resultField.textContent = ""
         }
         if (button.classList.contains("divide")) {
+            nums.push(parseInt(resultField.textContent))
             operator = "/"
-            resultField.textContent = operator
+            resultField.textContent = ""
         }
         if (button.classList.contains("clear")){
-            nums = []
-            operator = []
-            resultField.textContent = "0"
+            clear(0)
+            root.style.setProperty("--result-content", resultContent)
         } 
         if (button.classList.contains("operate")){
+            nums.push(parseInt(resultField.textContent))
             operate(nums, operator)
         }else if (!button.classList.contains("add") &&
                     !button.classList.contains("subtract") &&
@@ -35,8 +42,9 @@ buttons.forEach(button => {
                     !button.classList.contains("divide") &&
                     !button.classList.contains("clear"))
                     {
-                        nums.push(parseInt(button.textContent))
-                        resultField.textContent = button.textContent
+                        resultContent = " "
+                        root.style.setProperty("--result-content", resultContent)
+                        resultField.textContent += button.textContent
                     }
     })
 })
@@ -51,35 +59,53 @@ buttons.forEach(button => {
 })
 
 
-
-
-function add (a, b) {
-    return a + b
+function add (arr) {
+    return arr.reduce((total, el) => (total + el), 0)
 }
 
-function subtract (a, b) {
-    return a - b
+function subtract (arr) {
+    return arr.reduce((total, el) => (total - el))
 }
 
-function multiply (a, b) {
-    return a * b
+function multiply (arr) {
+    return arr.reduce((total, el) => (total * el), 1)
 }
 
-function divide (a, b) {
-    return a / b
+function divide (arr) {
+    return arr.reduce((total, el) => (total / el))
+}
+
+function clear(condition) {
+    /* condition 0 clears all */
+    if (condition === 0){
+        nums = []
+        operator = ""
+        resultField.textContent = ""
+        resultContent = ""
+    } 
+    /* condition 1 clears only nums */
+    else if (condition === 1){
+        nums = []
+        operator = ""
+    }
 }
 
 function operate(arr, operator) {
+    console.log(nums)
     if (operator == "+"){
-        resultField.textContent = add(arr[0], arr[1])
+        resultField.textContent = add(nums)
+        clear(1)
     }
     if (operator == "*"){
-        resultField.textContent = multiply(arr[0], arr[1])
+        resultField.textContent = multiply(nums)
+        clear(1)
     }
     if (operator == "-"){
-        resultField.textContent = subtract(arr[0], arr[1])
+        resultField.textContent = subtract(nums)
+        clear(1)
     }
     if (operator == "/"){
-        resultField.textContent = divide(arr[0], arr[1])
+        resultField.textContent = divide(nums)
+        clear(1)
     }
 }
