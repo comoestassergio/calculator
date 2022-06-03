d4fdf87f1eca16c073b611f6213dabd25ac30939
sergio <dirtycrew666@gmail.com>
2022-06-03 12:05:45 +0500

    Add ability to perform diff types of operations in a row

--- script.js
+++ script.js
@@ -3,31 +3,26 @@ const resultField = document.querySelector(".result")
 const root = document.documentElement
 
 let nums = []
-let operator
+let operator = []
 let resultContent = " " 
 
 buttons.forEach(button => {
     button.addEventListener("click", function(){
         if (button.classList.contains("add")) {
-            nums.push(parseInt(resultField.textContent))
-            console.log(nums)
-            operator = "+"
-            resultField.textContent = ""
+            pushNumsOperator(button)
+            checkNums()
         }
         if (button.classList.contains("subtract")) {
-            nums.push(parseInt(resultField.textContent))
-            operator = "-"
-            resultField.textContent = ""
+            pushNumsOperator(button)
+            checkNums()
         }
         if (button.classList.contains("multiply")) {
-            nums.push(parseInt(resultField.textContent))
-            operator = "*"
-            resultField.textContent = ""
+            pushNumsOperator(button)
+            checkNums()
         }
         if (button.classList.contains("divide")) {
-            nums.push(parseInt(resultField.textContent))
-            operator = "/"
-            resultField.textContent = ""
+            pushNumsOperator(button)
+            checkNums()
         }
         if (button.classList.contains("clear")){
             clear(0)
@@ -35,7 +30,7 @@ buttons.forEach(button => {
         } 
         if (button.classList.contains("operate")){
             nums.push(parseInt(resultField.textContent))
-            operate(nums, operator)
+            operate()
         }else if (!button.classList.contains("add") &&
                     !button.classList.contains("subtract") &&
                     !button.classList.contains("multiply") &&
@@ -44,10 +39,17 @@ buttons.forEach(button => {
                     {
                         resultContent = " "
                         root.style.setProperty("--result-content", resultContent)
-                        resultField.textContent += button.textContent
+                        if (resultField.textContent == nums[0]){
+                            resultField.textContent = ""
+                            resultField.textContent += button.textContent
+                        } else {
+                            resultField.textContent += button.textContent
+                        }
                     }
     })
 })
+
+/* handles button on-click animations */
 
 buttons.forEach(button => {
     button.addEventListener("click", function(){
@@ -58,6 +60,39 @@ buttons.forEach(button => {
     })
 })
 
+
+/* 
+    pushes the number from the result field to the nums array
+    pushes the current operator to the operator array
+ */
+
+function pushNumsOperator(button){
+    nums.push(parseInt(resultField.textContent))
+    operator.push(button.textContent)
+}
+
+/* 
+    checks if the nums array contains 2 numbers already, 
+    if so, it calculates these numbers using the first operator
+    from the operator array, then pushes the result into the array
+
+    if nums length is less than 2, it clears the result field
+
+ */
+
+
+function checkNums() {
+    if (nums.length > 1) {
+        operate()
+        nums.push(parseInt(resultField.textContent))
+    } else {
+        resultField.textContent = ""
+    }
+}
+
+/* 
+    all the calculator operations
+*/
 
 function add (arr) {
     return arr.reduce((total, el) => (total + el), 0)
@@ -79,32 +114,32 @@ function clear(condition) {
     /* condition 0 clears all */
     if (condition === 0){
         nums = []
-        operator = ""
+        operator = []
         resultField.textContent = ""
         resultContent = ""
     } 
-    /* condition 1 clears only nums */
+    /* condition 1 clears only nums and deletes operator[0]*/
     else if (condition === 1){
         nums = []
-        operator = ""
+        let temp = operator.shift()
     }
 }
 
-function operate(arr, operator) {
-    console.log(nums)
-    if (operator == "+"){
+function operate() {
+    let currentOperator = operator[0]
+    if (currentOperator == "+"){
         resultField.textContent = add(nums)
         clear(1)
     }
-    if (operator == "*"){
+    if (currentOperator == "*"){
         resultField.textContent = multiply(nums)
         clear(1)
     }
-    if (operator == "-"){
+    if (currentOperator == "-"){
         resultField.textContent = subtract(nums)
         clear(1)
     }
-    if (operator == "/"){
+    if (currentOperator == "/"){
         resultField.textContent = divide(nums)
         clear(1)
     }
